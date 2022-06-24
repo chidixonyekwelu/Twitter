ea288331da3ae533ff2626040f6fa24012edd2c0
Chidi Onyekwelu <chidixonyekwelu@fb.com>
2022-06-24 16:55:02 -0700

    Just the details view left

---
+++ twitter/TweetDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  TweetDetailsViewController.h
+//  twitter
+//
+//  Created by Chidi Onyekwelu on 6/24/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
